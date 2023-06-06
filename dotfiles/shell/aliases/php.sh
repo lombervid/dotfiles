5f7032d2235beab2bbed6d2f6fd98bfec7975a67
David Lombera <david.lomberlan@gmail.com>
2023-06-06 14:16:41 -0600

    add symfony aliases

--- dotfiles/shell/aliases/php.sh
+++ dotfiles/shell/aliases/php.sh
@@ -4,3 +4,7 @@ alias phpunit='./vendor/bin/phpunit'
 # Laravel
 alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
 alias artisan='php artisan'
+
+# Symfony
+alias sf='symfony'
+alias console='php bin/console'
