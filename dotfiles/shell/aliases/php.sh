b007f0f08b302523724062e5dba5e7ef02d2a084
David Lombera <david.lomberlan@gmail.com>
2023-06-09 16:36:41 -0600

    add alias for `PHPStan`

--- dotfiles/shell/aliases/php.sh
+++ dotfiles/shell/aliases/php.sh
@@ -1,6 +1,9 @@
 # PHPUnit
 alias phpunit='./vendor/bin/phpunit'
 
+# PHPStan
+alias phpstan='./vendor/bin/phpstan'
+
 # Laravel
 alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
 alias artisan='php artisan'
