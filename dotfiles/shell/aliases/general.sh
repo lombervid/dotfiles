9008cf3d126c2c8a2416a1d10bf83231860950eb
David Lombera <david.lomberlan@gmail.com>
2024-02-14 19:42:22 -0600

    add some changes
    - bspwm
    - kitty
    - picom
    - polybar
    - shell aliases
    - sxhkd

--- dotfiles/shell/aliases/general.sh
+++ dotfiles/shell/aliases/general.sh
@@ -13,6 +13,9 @@ alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
 alias fgrep='fgrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
 alias egrep='egrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
 
+# Get terminal name
+alias terminal='ps -p $PPID | tail -n 1 | awk '\''{print $NF}'\'
+
 # Neovim
 if (command_exists 'nvim'); then
     alias vi="nvim"
@@ -21,11 +24,11 @@ fi
 
 # Batcat
 if (command_exists 'bat'); then
-    alias cat='bat  --paging=never'
-    alias catp='bat  --paging=never -p'
-    alias catn='bat  --paging=never -n'
+    alias cat='bat --paging=never'
+    alias catp='bat --paging=never -p'
+    alias catn='bat --paging=never -n'
 elif (command_exists 'batcat'); then
-    alias cat='batcat  --paging=never'
-    alias catp='batcat  --paging=never -p'
-    alias catn='batcat  --paging=never -n'
+    alias cat='batcat --paging=never'
+    alias catp='batcat --paging=never -p'
+    alias catn='batcat --paging=never -n'
 fi
