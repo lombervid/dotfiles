56c1138f6359a64bd44d81c4b89c52ca07a8492d
lombervid <david.lomberlan@gmail.com>
2025-12-07 15:55:54 -0600

    niri: update close script to close the correct window

--- dotfiles/niri/scripts/close-window.sh
+++ dotfiles/niri/scripts/close-window.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 
-WINDOW=$(niri msg focused-window | grep "App ID:" | awk -F ': ' '{print $2}')
-if [ "${WINDOW}" == "" ]; then
+set -euo pipefail
+IFS=$'\n\t'
+
+read -r WINDOW_ID WINDOW_TITLE < <(niri msg -j focused-window | jq -r '[.id, .app_id] | @tsv')
+if [ "${WINDOW_TITLE}" == "" ]; then
     exit;
 fi
 
-CHOICE=$(echo -e "Yes\nNo" | rofi -dmenu -no-custom -i -p "Close ${WINDOW}?")
+CHOICE=$(echo -e "Yes\nNo" | rofi -dmenu -no-custom -i -p "Close ${WINDOW_TITLE}?")
 if [ "${CHOICE}" == "Yes" ]; then
-    niri msg action close-window
+    niri msg action close-window --id "${WINDOW_ID}"
 fi
